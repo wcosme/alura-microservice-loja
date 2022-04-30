a0ed1506eeb867ad46acd0812c72311e614ff2bf
Wallace Gonçalves <wallacecosme@gmail.com>
2022-04-29 21:44:57 -0300

    Criando o RestTemplate client

---
+++ loja/src/main/java/br/com/microservice/loja/dtos/InfoFornecedorDTO.java
@@ -0,0 +1,10 @@
+package br.com.microservice.loja.dtos;
+
+import lombok.Data;
+
+@Data
+public class InfoFornecedorDTO {
+	
+	private String endereco;
+
+}
--- loja/src/main/java/br/com/microservice/loja/services/impl/CompraServiceImpl.java
+++ loja/src/main/java/br/com/microservice/loja/services/impl/CompraServiceImpl.java
@@ -1,8 +1,12 @@
 package br.com.microservice.loja.services.impl;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.microservice.loja.dtos.CompraDTO;
+import br.com.microservice.loja.dtos.InfoFornecedorDTO;
 import br.com.microservice.loja.services.CompraService;
 
 @Service
@@ -11,6 +15,9 @@ public class CompraServiceImpl implements CompraService {
 	@Override
 	public void realizaCompra(CompraDTO compraDTO) {
 		
+		RestTemplate client = new RestTemplate();
+		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange("http://localhost:8081/info/"+compraDTO.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
 		
+		System.out.println(exchange.getBody().getEndereco());
 	}
 }
