0e8fd6f4447369275bc9a9f8f2adecb508e8090a
Wallace Gonçalves <wallacecosme@gmail.com>
2022-05-02 18:27:15 -0300

    Implementando Circuit Break

--- loja/src/main/java/br/com/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/microservice/loja/LojaApplication.java
@@ -2,12 +2,14 @@ package br.com.microservice.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
--- loja/src/main/java/br/com/microservice/loja/services/impl/CompraServiceImpl.java
+++ loja/src/main/java/br/com/microservice/loja/services/impl/CompraServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.microservice.loja.clilent.FornecedorClient;
 import br.com.microservice.loja.dtos.CompraDTO;
 import br.com.microservice.loja.dtos.InfoFornecedorDTO;
@@ -17,6 +19,7 @@ public class CompraServiceImpl implements CompraService {
 	@Autowired
 	private FornecedorClient client;
 
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
 	@Override
 	public Compra realizaCompra(CompraDTO compraDTO) {
 		
@@ -31,5 +34,14 @@ public class CompraServiceImpl implements CompraService {
 		BeanUtils.copyProperties(pedido, compraRealizada);
 					
 		return compraRealizada;
+	}
+	
+	public Compra realizaCompraFallback(CompraDTO compraDTO) {
+		
+		var compraFallback = new Compra();
+		compraFallback.setEnderecoDestino(compraDTO.getEndereco().toString());
+		
+		return compraFallback;
+		
 	}
 }
